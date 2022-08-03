3184668458edac67e106469f2194ba94fad8a1bc
Joel <franco4prez@gmail.com>
2022-08-03 15:45:56 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Joel";
+            int age = 24;
+            char grade = 'A';
+            bool cat = true;
+            double price = 14.99D;
+            decimal usaGDP = 20939999999.99M;
+
+            Console.WriteLine("My name is " + firstName + ". I am " + age + " years old.");
+            Console.WriteLine("When I was in school, I was told to get " + grade + " on my report card.");
+            Console.WriteLine("Currently it is: " + cat + " that I have a cat.");
+            Console.WriteLine("My grocery bill last week was $" + price + ". This week it was $" + usaGDP + ". Inflation is crazy.");
+        }
+    }
+}
